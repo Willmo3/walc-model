d0165d580e8c94d7b1232b7c3eca46ed23b31459
Will Morris <morris.william.work@gmail.com>
2025-02-03 15:00:32 -0500

    Move frontend to walc-model

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,50 @@
+use crate::bytecode::bytecode_generator;
+use crate::frontend::{lexer, parser};
+
 /// Bytecode support for the walc programming language.
 /// Author: Will Morris
-pub mod bytecode {
+mod bytecode {
     pub mod bytecode_interpreter;
     pub mod bytecode_generator;
 }
 
 /// Walc AST operations, including treewalk interpreter.
 /// Author: Will Morris
-pub mod ast {
+mod ast {
     pub mod ast;
     pub mod treewalk_interpreter;
 }
 
-pub mod frontend {
+/// Frontend for the Walc interpreter.
+/// Author: Will Morris
+mod frontend {
     pub mod lexer;
     pub mod parser;
+}
+
+/// Interface for Walc language. Takes a source program and returns the result of the computation,
+/// Or an error.
+pub fn interpret(source_code: &str) -> Result<f64, String> {
+    let tokens = lexer::lex(source_code);
+    // Note: why does the parser consume the tokens? Look into changing.
+    let ast = parser::parse(tokens).unwrap();
+    let bytecode = bytecode_generator::generate(&ast);
+    bytecode::bytecode_interpreter::interpret(&bytecode)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::interpret;
+
+    #[test]
+    fn test_interpret_div() {
+        let source = "(3 + 5 + 2 - 2) * 2 / 8";
+        assert_eq!(Ok(2.0), interpret(source));
+    }
+
+    #[test]
+    fn test_div_zero() {
+        let source = "1 / 0";
+        assert_eq!(Err("Cannot divide by zero.\nNo result.\n".to_string()), interpret(source));
+    }
 }
