f2c77bbd8da9d1afac719f6534c756f7779bc2b4
willmo3 <thelordofbouge@gmail.com>
2025-08-19 15:32:41 -0400

    Update interpreter to execute readvalues of scoped variables

--- src/bytecode/bytecode_interpreter.rs
+++ src/bytecode/bytecode_interpreter.rs
@@ -1,8 +1,9 @@
 use std::error::Error;
 use crate::bytecode::opcode::Opcode;
-use crate::bytecode::opcode::Opcode::{ADD, DIVIDE, EXP, MULTIPLY, PUSH, SUBTRACT, ASSIGN };
+use crate::bytecode::opcode::Opcode::{ADD, DIVIDE, EXP, MULTIPLY, PUSH, SUBTRACT, ASSIGN, READVAR };
 use crate::bytecode::stackframe::Binding;
 use std::str;
+use std::str::Utf8Error;
 
 const IMM_LEN: usize = 8;
 
@@ -20,106 +21,147 @@ pub fn execute(bytes: &Vec<u8>) -> Result<f64, String> {
 
     // Begin interpreting from the program's root scope, recursively descending lower.
     let mut root_frame = Binding::new();
-    match interpret_scope(&mut state, &mut stack, &mut root_frame) {
+    match state.interpret_scope(&mut stack, &mut root_frame) {
         true => Ok(stack.pop().unwrap()),
         false => Err(state.errors),
     }
 }
 
-/*
- * Interpret a single level of program scope, recursing if a new level of depth found.
- * Returns whether frame resulted in bad result.
- */
-fn interpret_scope(state: &mut InterpreterState,
-                   stack: &mut Vec<f64>,
-                   scope_var_binding: &mut Binding) -> bool {
 
-    while state.index < state.code.len() {
-        let operation = state.code[state.index];
-        match Opcode::opcode_from_byte(operation) {
-            ASSIGN => {
-                state.index += 1; // Skip opcode
+impl InterpreterState<'_> {
+    /*
+     * Interpret a single level of program scope, recursing if a new level of depth found.
+     * Returns whether frame resulted in bad result.
+     */
+    fn interpret_scope(&mut self,
+                       stack: &mut Vec<f64>,
+                       scope_var_binding: &mut Binding) -> bool {
 
-                // Previous code must have produced value on stack to use.
-                if stack.is_empty() {
-                    state.errors.push_str("Assignment attempted without value!\n");
-                    continue
-                }
+        while self.index < self.code.len() {
+            let operation = self.code[self.index];
+            self.index += 1; // Skip opcode.
 
-                // The next field will be the length of the identifier.
-                let length = state.code[state.index] as usize;
-                state.index += 1;
+            // Collect errors for this operation.
+            let mut iteration_errors = String::new();
 
-                // Identifier must be comprised of well-formatted utf8 bytes.
-                // We do not support multi-byte characters yet.
-                let identifier = str::from_utf8(&state.code[state.index..(state.index + length)]);
-                match identifier {
-                    Ok(value) => {
-                        state.index += value.len();
-                        // NOTE: currently, assignments evaluate to whatever value was assigned.
-                        // Therefore, we do not bother popping the value from the stack.
-                        scope_var_binding.set_bind(String::from(value), *stack.first().unwrap())
+            // TODO: unclear that each state can have its own code without some pre-facto analysis.
+            // More realistic: bound each scope to names in the higher scope
+            // Don't need each scope to have its own code.
+            match Opcode::opcode_from_byte(operation) {
+                ASSIGN => {
+                    // Previous code must have produced value on stack to use.
+                    if stack.is_empty() {
+                        iteration_errors.push_str("Assignment attempted without value!\n");
+                        continue
                     }
-                    Err(e) => {
-                        state.index += e.valid_up_to();
-                        state.errors.push_str("Bytecode UTF conversion error. Expected: stream of valid UTF8 bytes for identifier.\n");
-                        // Skip past the valid parts of the string and continue parsing from the illegal index.
-                    }
-                }
-            },
-            PUSH => {
-                state.index += 1; // Skip opcode.
 
-                let mut immediate_bytes = [0u8; IMM_LEN];
-                immediate_bytes[..IMM_LEN].copy_from_slice(
-                    &state.code[state.index..(state.index + IMM_LEN)]);
-
-                stack.push(f64::from_le_bytes(immediate_bytes));
-                state.index += IMM_LEN; // Read 8-bytes from bytecode value.
-            },
-            ADD | SUBTRACT | MULTIPLY | DIVIDE | EXP => {
-                state.index += 1; // Skip opcode
-
-                if stack.len() < 2 {
-                    state.errors.push_str("Binary operation attempted with insufficient operands!\n");
-                    continue
-                }
-
-                // Operands pushed onto stack in reverse order.
-                let right = stack.pop().unwrap();
-                let left = stack.pop().unwrap();
-
-                match Opcode::opcode_from_byte(operation) {
-                    ADD => stack.push(left + right),
-                    SUBTRACT => stack.push(left - right),
-                    MULTIPLY => stack.push(left * right),
-                    DIVIDE => {
-                        if right == 0.0 {
-                            state.errors.push_str("Cannot divide by zero.\n");
-                            continue
+                    let identifier = self.read_identifier();
+                    match identifier {
+                        Ok(value) => {
+                            // NOTE: currently, assignments evaluate to whatever value was assigned.
+                            // Therefore, we do not bother popping the value from the stack.
+                            scope_var_binding.set_bind(String::from(value), *stack.first().unwrap())
                         }
-                        stack.push(left / right)
-                    },
-                    EXP => stack.push(left.powf(right)),
-                    _ => state.errors.push_str(&format!("Unknown binary operation: {}\n", operation)),
+                        Err(e) => {
+                            // Skip past the valid parts of the string and continue interpreting from the illegal index.
+                            // Maybe it was an instruction we can execute?
+                            // TODO: security problem, rethink approach.
+                            self.index += e.valid_up_to();
+                            self.errors.push_str("Bytecode UTF conversion error. Expected: stream of valid UTF8 bytes for identifier.\n");
+                        }
+                    }
+                },
+                READVAR => {
+                    let identifier = self.read_identifier();
+                    match identifier {
+                        Ok(name) => {
+                            if let Some(value) = scope_var_binding.get_bind(name) {
+                                stack.push(*value);
+                            } else {
+                                iteration_errors.push_str(format!("Variable {} not found!\n", name).as_str());
+                            }
+                        }
+                        Err(e) => {
+                            self.index += e.valid_up_to();
+                            iteration_errors.push_str("Bytecode UTF conversion error. Expected: stream of valid UTF8 bytes for identifier.\n");
+                        }
+                    }
+                }
+                PUSH => {
+                    let mut immediate_bytes = [0u8; IMM_LEN];
+                    immediate_bytes[..IMM_LEN].copy_from_slice(
+                        &self.code[self.index..(self.index + IMM_LEN)]);
+
+                    stack.push(f64::from_le_bytes(immediate_bytes));
+                    self.index += IMM_LEN; // Read 8-bytes from bytecode value.
+                },
+                ADD | SUBTRACT | MULTIPLY | DIVIDE | EXP => {
+                    if stack.len() < 2 {
+                        iteration_errors.push_str("Binary operation attempted with insufficient operands!\n");
+                        continue
+                    }
+
+                    // Operands pushed onto stack in reverse order.
+                    let right = stack.pop().unwrap();
+                    let left = stack.pop().unwrap();
+
+                    match Opcode::opcode_from_byte(operation) {
+                        ADD => stack.push(left + right),
+                        SUBTRACT => stack.push(left - right),
+                        MULTIPLY => stack.push(left * right),
+                        DIVIDE => {
+                            if right == 0.0 {
+                                iteration_errors.push_str("Cannot divide by zero.\n");
+                            } else {
+                                stack.push(left / right)
+                            }
+                        },
+                        EXP => stack.push(left.powf(right)),
+                        _ => iteration_errors.push_str(&format!("Unknown binary operation: {}\n", operation)),
+                    }
                 }
             }
+            // Update the list of all errors with the issues in this execution.
+            self.errors.push_str(iteration_errors.as_str());
         }
-    }
 
-    if stack.len() == 0 {
-        state.errors.push_str("No result.\n");
-    }
+        if stack.len() == 0 {
+            self.errors.push_str("No result.\n");
+        }
 
-    // If any errors have been detected, an interpretation round is tainted.
-    state.errors.is_empty()
+        // If any errors have been detected, an interpretation round is tainted.
+        self.errors.is_empty()
+    }
+}
+
+/*
+ * Interpretation helpers.
+ */
+impl InterpreterState<'_> {
+
+    /**
+     * Read an identifier name from the top of the program stack.
+     * If an identifier was read, move the program counter past its last byte.
+     * Return whether the identifier name was successfully converted into a UTF8 string.
+     */
+    fn read_identifier(&mut self) -> Result<&str, Utf8Error> {
+        // Length of identifier.
+        let length = self.code[self.index] as usize;
+        self.index += 1;
+
+        let identifier = str::from_utf8(&self.code[self.index..(self.index + length)]);
+        if let Ok(identifier) = identifier {
+            self.index += identifier.len();
+        }
+        identifier
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::bytecode::bytecode_interpreter::execute;
     use crate::bytecode::opcode::Opcode;
-    use crate::bytecode::opcode::Opcode::{ASSIGN, MULTIPLY, SUBTRACT};
+    use crate::bytecode::opcode::Opcode::{ASSIGN, DIVIDE, MULTIPLY, PUSH, READVAR, SUBTRACT};
 
     #[test]
     fn test_add() {
@@ -223,7 +265,7 @@ mod tests {
     }
 
     #[test]
-    fn test_assign() {
+    fn test_assign_access() {
         // value_a = 2 - 3 * 2
         let mut code = Vec::new();
 
@@ -246,6 +288,23 @@ mod tests {
         code.push(identifier.len() as u8);
         code.extend_from_slice(identifier.as_bytes());
 
+        // An assignment evaluates to the lval that it produces.
         assert_eq!(execute(&code).unwrap(), -4.0);
+
+        // Additionally, we can dereference that lval.
+        code.push(Opcode::byte_from_opcode(&READVAR));
+        code.push(identifier.len() as u8);
+        code.extend_from_slice(identifier.as_bytes());
+
+        // This code with a READVAR instruction should also evaluate to -4.0.
+        assert_eq!(execute(&code).unwrap(), -4.0);
+
+        // We can also run an arithmetic operation on this result!
+        code.push(Opcode::byte_from_opcode(&PUSH));
+        code.extend_from_slice(&f64::to_le_bytes(32.0));
+
+        code.push(Opcode::byte_from_opcode(&DIVIDE));
+
+        assert_eq!(execute(&code).unwrap(), -0.125);
     }
 }
